722ea3067091e2f2bf60a5115ad792b8d640af59
ha-takahashi <ha-takahashi@kddi.com>
2019-04-02 13:42:33 +0900

    Modify travis_package_make.bash for install ros-kinetic-cv-bridge ros-kinetic-cv-camera ros-kinetic-image-transport-plugins opencv-data

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,9 @@
 #!/bin/bash -xve
 
 sudo apt-get update
+sudo apt install ros-kinetic-cv-bridge
+sudo apt install ros-kinetic-cv-camera
+sudo apt install ros-kinetic-image-transport-plugins
 sudo apt-get install libopencv-dev python-opencv
 
 sudo apt-get install ros-indigo-cv-bridge
@@ -15,3 +18,4 @@ git clone --depth=1 https://github.com/hirotaka001/pimouse_ros.git
 
 cd ~/catkin_ws
 catkin_make
+sudo apt install opencv-data
