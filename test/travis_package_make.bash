4c57a295f82105034ad2e805c8e60c309f288df7
ha-takahashi <ha-takahashi@kddi.com>
2019-05-03 23:05:19 +0900

    Modify travis_package_make.bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,9 +1,9 @@
 #!/bin/bash -xve
 
 sudo apt-get update
-sudo apt-get -f install ros-kinetic-cv-bridge
-sudo apt-get -f install ros-kinetic-cv-camera
-sudo apt-get -f install ros-kinetic-image-transport-plugins
+#sudo apt-get -f install ros-kinetic-cv-bridge
+#sudo apt-get -f install ros-kinetic-cv-camera
+#sudo apt-get -f install ros-kinetic-image-transport-plugins
 sudo apt-get install libopencv-dev python-opencv
 
 sudo apt-get install ros-indigo-cv-bridge
@@ -14,8 +14,8 @@ rsync -av ./ ~/catkin_ws/src/pimouse_vision_control/
 
 #clone pimouse_ros
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/hirotaka001/pimouse_ros.git
+git clone https://github.com/hirotaka001/pimouse_ros.git
 
 cd ~/catkin_ws
 catkin_make
-sudo apt install opencv-data
+#sudo apt install opencv-data
